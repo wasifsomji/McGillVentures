ae05cc9e58cfce5182365671b05d8d64989724e5
sandynguyen8928 <sandynguyen8928@gmail.com>
2023-07-02 23:13:12 -0400

    added quote + who we are sections and created footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,52 @@
+import styles from "./Footer.module.scss"
+import baseStyle from "../../styles/Base.module.scss";
+import logo from "../../assets/unicorn_logo.png";
+import {Stack, Typography} from "@mui/material";
+
+const Footer = () => {
+    return (
+        <div className={styles.footerBackground}>
+            <Stack className={styles.copyrightsContainer}>
+                <img src={logo} className={baseStyle.UnicornLogo} alt="Company Logo"/>
+                <Typography
+                    sx={{
+                    mr: 2,
+                    display: 'flex',
+                    fontFamily: 'Helvetica Neue',
+                    color: 'white',
+                    textDecoration: 'none',
+                    fontWeight: 700,
+                    fontSize: { xs: '12px', md: '16px' }}}
+                >© 2023 McGill Ventures</Typography>
+                <Typography
+                    sx={{
+                        mr: 2,
+                        display: 'flex',
+                        fontFamily: 'Helvetica Neue',
+                        color: 'white',
+                        textDecoration: 'none',
+                        fontSize: { xs: '12px', md: '16px' }}}
+                >Proudly part of McGill’s Management Undergraduate Society</Typography>
+            </Stack>
+
+            <div className={styles.rightLinks}>
+                <div className={styles.linksContainer}>
+                    <div className={styles.linksTitle}>Quick Links</div>
+                    <a className={styles.linksText} href="www.google.com">Home</a>
+                    <a className={styles.linksText} href="www.google.com">About</a>
+                    <a className={styles.linksText} href="www.google.com">Events</a>
+                    <a className={styles.linksText} href="www.google.com">Contact</a>
+                </div>
+                <div className={styles.linksContainer}>
+                    <div className={styles.linksTitle}>Follow Us</div>
+                    <a className={styles.linksText} href="https://www.facebook.com/mcgillventuresclub" target="_blank" rel="noreferrer">Facebook</a>
+                    <a className={styles.linksText} href="https://www.instagram.com/mcgillvc/" target="_blank" rel="noreferrer">Instagram</a>
+                    <a className={styles.linksText} href="https://www.linkedin.com/company/mcgill-ventures/" target="_blank" rel="noreferrer">LinkedIn</a>
+                    <a className={styles.linksText} href="www.google.com" target="_blank" rel="noreferrer">Website</a>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Footer;
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -4,6 +4,9 @@ import NavBar from "../../components/NavBar/NavBar";
 import styles from "./Home.module.scss"
 import Landing from "../../components/Landing/Landing";
 import landingImage from "../../assets/mcgill.jpeg";
+import coverImage from "../../assets/mcgill.jpeg";
+import whoWeAreImage from "../../assets/mcgill.jpeg";
+import Footer from "../../components/Footer/Footer";
 
 const Home = () => {
     return (
@@ -12,6 +15,44 @@ const Home = () => {
             <Landing title="Building Bridges to Tomorrow’s Innovators"
                      subtitle="We aim to educate students on Venture Capital and Private Equity."
                      image={landingImage}/>
+            <div className={styles.quoteContainer}>
+                <div className={styles.quoteText}>
+                    “We need to start to talk about money in ways that dethrone it and make it subject to human ethics and standards of love and decency.”
+                </div>
+                <div className={styles.quoteAuthor}>
+                    — Joel Solomon, The Clean Money Revolution: Reinventing Power, Purpose and Capitalism
+                </div>
+            </div>
+            <img src={coverImage} className={styles.coverImage} />
+            <div className={styles.whoWeAreContainer}>
+                <div className={styles.titleText}>
+                    <div className={styles.whoWeAreTitle}>
+                        Who We Are
+                    </div>
+                    <div className={styles.whoWeAreText}>
+                        Founded in 2020, McGill Ventures is a dynamic student-run organization based in Montreal, Quebec.
+                        Comprised of McGill students passionate about venture capital and private equity, our club offers a supportive community and valuable opportunities for learning and networking.
+                        If you are interested in joining our club, collaborating on projects, or learning more about the venture capital and private equity landscape, we encourage you to reach out to us.
+                        McGill Ventures looks forward to connecting with you and supporting your journey in this exciting field.
+                    </div>
+                </div>
+                <div className={styles.titleText}>
+                    <div className={styles.whoWeAreTitle}>
+                        What We Do
+                    </div>
+                    <div className={styles.whoWeAreText}>
+                        We aim to educate students on Venture Capital and Private Equity, providing a dynamic platform for learning, networking, and exploring the ever-evolving landscape of investment opportunities.
+                        Through engaging events, industry insights, and hands-on experiences, we empower aspiring investors to navigate the realm of innovation, fostering an ecosystem where bold ideas thrive and promising ventures take flight.
+                        Join us as we bridge the gap between academia and entrepreneurship, unlocking a world of possibilities for tomorrow's visionary leaders.
+                    </div>
+                </div>
+                <div className={styles.whoWeAreImagesContainer}>
+                    <img src={whoWeAreImage} className={styles.whoWeAreImage}/>
+                    <img src={whoWeAreImage} className={styles.whoWeAreImage}/>
+                    <img src={whoWeAreImage} className={styles.whoWeAreImage}/>
+                </div>
+            </div>
+            <Footer />
         </div>
     )
 }
