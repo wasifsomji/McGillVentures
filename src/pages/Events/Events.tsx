d074d1ac10456dbd85c6a169c097e914076a3ad8
wasifsomji <wasifsomji@gmail.com>
2024-11-18 13:25:16 -0500

    added summit event

--- src/pages/Events/Events.tsx
+++ src/pages/Events/Events.tsx
@@ -4,6 +4,7 @@ import NavBar from "../../components/NavBar/NavBar";
 import styles from "./Events.module.scss";
 import futureEvent from "../../assets/events/navigating_ai.jpg";
 import futureEvent1 from "../../assets/events/intro_vc_panel.jpg";
+import futureEvent2 from "../../assets/events/ventures-summit.jpg";
 import eventImage2 from "../../assets/events/venture_capital_and_beyond.png";
 import Event from "../../components/Event/Event";
 import Footer from "../../components/Footer/Footer";
@@ -13,50 +14,6 @@ import scarlettPitch1 from "../../assets/events/Scarlet_Pitch_img.png";
 import highlight2 from "../../assets/event_highlights_24-25/img3.jpg";
 import higlight1 from "../../assets/event_highlights_24-25/img1.jpg";
 import highlight3 from "../../assets/event_highlights_24-25/img4.jpg";
-
-// const Description_startupShowcaseWithVCInvestors =
-//     <div>
-//         McGill Ventures is excited to announce its first event of the year at the historic Thomson House venue: “Startup Showcase with VC Investors” 🚀
-//         <br /><br />
-//         Seize this exclusive opportunity to meet and learn from local entrepreneurs and venture capitalists. Discover the missions, product differentiators, and future plans of the startup founders, as well as the investment perspectives and roles of the venture capitalists 📈
-//         <br /><br />
-//         Tickets are limited, so secure yours now! Register through the link in our bio or at:
-//         🔗 https://startupshowcase.eventbrite.com/
-//         <br /><br />
-//         <b>WHAT TO EXPECT</b><br />
-//         🌟 Network with ambitious individuals to jumpstart your career in business/finance.
-//         <br />🧠 Gain tangible insights into the realms of entrepreneurship/venture capital through presentations.
-//         <br />🥪 Enjoy free appetizers and bites, included with your ticket.
-//         <br /><br />
-//         <b>WHAT TO KNOW</b><br />
-//         👔 Business Casual dress code.
-//         <br />📅 Friday, October 27th, 1:30 PM to 4:00 PM.
-//         <br />📍 PGSS Inc. Thomson House (Ballroom, 2nd Floor), 3650 McTavish St, Montreal.
-//         <br /><br />
-//         See you soon! 👋
-//     </div>
-
-// const Description_vcPanelAndNetworkingEventAtEspaceCDPQ =
-//   <div>
-//     McGill Ventures is thrilled to announce an event downtown at the dynamic Espace CDPQ: “Venture Capitalist Panel & Networking at Espace CDPQ: Innovation in Media & Entertainment” 🚀
-//     <br /><br />
-//     The panel discussion will focus on the current landscape of venture capital and innovation in the media & entertainment industry 📈 It will feature VCs from Triptyq Capital and Panache Ventures, as well as the CTO/CIO of La Presse 📰 This is also a fantastic opportunity to visit Espace CDPQ, Canada's largest venture capital community 🏢
-//     <br /><br />
-//     Tickets are extremely limited and registration ends in a few days - don't miss out! Secure your spot now via the link in our bio or at:
-//     🔗 https://bit.ly/vcpanelcdpq
-//     <br /><br />
-//     <b>WHAT TO EXPECT</b><br />
-//     🌟 Expand your professional network within the venture capital/investment ecosystem and connect with like-minded students.
-//     🧠 Gain tangible insights into the realms of venture capital, entrepreneurship, and media & entertainment.
-//     🥤 Enjoy free snacks and beverages, included with your ticket.
-//     <br /><br />
-//     <b>WHAT TO KNOW</b><br />
-//     👔 Business casual dress code.
-//     📅 Friday, November 10th, 2:00 - 5:00 PM.
-//     📍 3, Place Ville Marie, bureau 12350 (Salle CDPQ at Espace CDPQ), Montreal, QC H3B 0E7.
-//     <br /><br />
-//     See you there! 👋
-//   </div>
 
 const Events = () => {
     return (
@@ -89,6 +46,15 @@ const Events = () => {
                             location="Brofman Building"
                             image={futureEvent}
                             link="https://www.eventbrite.com/e/ethics-of-innovation-navigating-responsiblity-in-ai-driven-startups-tickets-1073146924339?aff=oddtdtcreator"
+                        />
+                    </ScrollAnimation>
+                    <ScrollAnimation animateIn="fadeInUp" duration="1.5" animateOnce="true">
+                        <Event
+                            title="Ventures Summit"
+                            date="November 29, 2024"
+                            location="Espace CDPQ"
+                            image={futureEvent2}
+                            link="https://www.eventbrite.ca/e/ventures-summit-tickets-1057814233849?aff=oddtdtcreator"
                         />
                     </ScrollAnimation>
                 </div>
