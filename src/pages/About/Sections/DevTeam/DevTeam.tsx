0a57de5bbad502f60c1d3125d245351c2545b92d
wasifsomji <wasifsomji@gmail.com>
2024-12-04 16:16:48 -0500

    added 4 devs

--- src/pages/About/Sections/DevTeam/DevTeam.tsx
+++ src/pages/About/Sections/DevTeam/DevTeam.tsx
@@ -8,6 +8,11 @@ import ScrollAnimation from 'react-animate-on-scroll';
 
 import somji from "../../../../assets/exec_headshots/Wasif Somji (Dtech).jpeg";
 
+import joseph from "../../../../assets/devTeam/darlie_joseph_headshot.jpg";
+import dmytro from "../../../../assets/devTeam/dmytro_martyniuk_headshot.png";
+import hum from "../../../../assets/devTeam/jaden_hum_headshot.jpg";
+import heng from "../../../../assets/devTeam/julien_heng_headshot.jpg";
+
 
 
 const ExecTeam = () => {
@@ -19,6 +24,20 @@ const ExecTeam = () => {
                 </ScrollAnimation>
                 <ScrollAnimation animateIn="fadeInUp" duration="1.5" animateOnce="true">
                     <TeamMember image={person1} name="Yassine Mimet" role="Associate Product Manager" major="Computer Engineering" hometown="Tetouan, Morocco" grad="2025"/>
+                </ScrollAnimation>
+            </div>
+            <div className={styles.teamInnerContainer}>
+                <ScrollAnimation animateIn="fadeInUp" duration="1.5" animateOnce="true">
+                    <TeamMember image={dmytro} name="Dmytro Martyniuk" role="Backend Developer" major="Software Engineering" hometown="Montreal, QC" grad="2026"/>
+                </ScrollAnimation>
+                <ScrollAnimation animateIn="fadeInUp" duration="1.5" animateOnce="true">
+                    <TeamMember image={heng} name="Julien Heng" role="Fullstack Developer" major="Software Engineering" hometown="Paris, France" grad="2027"/>
+                </ScrollAnimation>
+                <ScrollAnimation animateIn="fadeInUp" duration="1.5" animateOnce="true">
+                    <TeamMember image={hum} name="Jaden Hum" role="Frontend Developer" major="Software Engineering" hometown="Montreal, QC" grad="2025"/>
+                </ScrollAnimation>
+                <ScrollAnimation animateIn="fadeInUp" duration="1.5" animateOnce="true">
+                    <TeamMember image={joseph} name="Darlie Joseph" role="Product Designer" major="Psychology" hometown="Montreal, QC"/>
                 </ScrollAnimation>
             </div>
         </div>
