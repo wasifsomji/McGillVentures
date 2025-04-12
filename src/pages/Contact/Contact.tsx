cbdb3624bad319a43d9763d878971e1a5cbf0a5c
wasifsomji <wasifsomji@gmail.com>
2025-04-12 12:00:06 -0400

    re trigger ci

--- src/pages/Contact/Contact.tsx
+++ src/pages/Contact/Contact.tsx
@@ -14,9 +14,6 @@ import { useState } from "react";
 const supabaseUrl = process.env.REACT_APP_SUPABASE_URL!;
 const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY!;
 
-console.log('supabaseUrl:', supabaseUrl);
-console.log('supabaseAnonKey:', supabaseAnonKey);
-
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
 const Contact = () => {
